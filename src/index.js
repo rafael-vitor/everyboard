1ef9a614d5ffcdd202c462ac8caca66e297c068e
Gabriel Genê <gabrielgene97@gmail.com>
2018-01-05 01:58:56 -0300

    Add todo list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoList from './modules/todo-list';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
 const App = () => (
-  <h1>Start</h1>
+  <TodoList />
 )
 
 ReactDOM.render(<App />, document.getElementById('root'));
