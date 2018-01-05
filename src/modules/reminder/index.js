87a5741924a5d37cdfeac0f939078e1bacc466a3
Gabriel Genê <gabrielgene97@gmail.com>
2018-01-05 18:35:03 -0300

    Create reminder module

---
+++ src/modules/reminder/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Reminder = () => (
+  <h1>Reminder</h1>
+);
+
+export default Reminder;
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Reminder from '../../modules/reminder';
 import TodoList from '../../modules/todo-list';
 import Base from './Base';
 
 const Dashboard = () => (
   <Base>
+    {/* <Reminder /> */}
     <TodoList />
   </Base>
 );
