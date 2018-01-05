0d6061ce36c6ffa5323fecd1b7047d86231d75b8
Gabriel Genê <gabrielgene97@gmail.com>
2018-01-05 16:13:35 -0300

    Add Dashboard

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodoList from '../../modules/todo-list';
+import Base from './Base';
+
+const Dashboard = () => (
+  <Base>
+    <TodoList />
+  </Base>
+);
+
+export default Dashboard;
