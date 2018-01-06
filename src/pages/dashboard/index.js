d272989a09f8f28773b61da19948e3b2cd47cf2a
Gabriel Genê <gabrielgene97@gmail.com>
2018-01-06 00:37:44 -0300

    Implement note

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Reminder from '../../modules/reminder';
 import TodoList from '../../modules/todo-list';
+import Note from '../../modules/note';
 import Base from './Base';
 
 const Dashboard = () => (
   <Base>
     {/* <Reminder /> */}
-    <TodoList />
+    {/* <TodoList /> */}
+    <Note />
   </Base>
 );
 
